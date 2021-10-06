07d74051d425a0fe063a3b6eedfd34fa41c3cefa
jieun1128 <5656jieun@naver.com>
2021-10-07 01:25:52 +0900

    feat: boto3 test

--- backend/run.py
+++ backend/run.py
@@ -1,11 +1,30 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template
+import boto3
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    boto()
+    return render_template('new.html')
+
+def boto():
+    ec2_client = boto3.client("ec2", region_name="ap-northeast-2")
+    reservations = ec2_client.describe_instances(Filters=[
+        {
+            "Name" : "instance-state-name",
+            "Values" : ["running"],
+        }
+    ]).get("Reservations")
+
+    for reservation in reservations:
+        for instance in reservation["Instances"]:
+            instance_id = instance["InstanceId"]
+            instance_type = instance["InstanceType"]
+            public_ip = instance["PublicIpAddress"]
+            private_ip = instance["PrivateIpAddress"]
+            print(f"{instance_id}, {instance_type}, {public_ip}, {private_ip}")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(port=3000, debug=True)
 
